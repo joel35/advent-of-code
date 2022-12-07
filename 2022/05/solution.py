0d15099f0c69184a87ca267120611260da9059f0
Joel <64959969+joel35@users.noreply.github.com>
2022-12-07 12:03:46 +0000

    day 5 finished

--- 2022/05/solution.py
+++ 2022/05/solution.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-#  https://adventofcode.com/2022/day/...
+#  https://adventofcode.com/2022/day/5
 
-import re
 
 FILE = "input.txt"
 
 
 def main():
     data = load_input()
-    print(f"PART 1: {solve_part_1(data)}")
-    print(f"PART 2: {solve_part_2(data)}")
+    p1_data = prepare_data(data)
+    print(f"PART 1: {solve_part_1(p1_data)}")
+    p2_data = prepare_data(data)
+    print(f"PART 2: {solve_part_2(p2_data)}")
 
 
 def load_input(file=FILE) -> list:
@@ -24,31 +25,75 @@ def prepare_data(data) -> tuple:
 
     for row in data[: split_index - 1]:
         row = row.replace("    ", " [.] ")
-        columns = [c.strip() for c in row.split(" ") if not c in ("", "\n")]
+        columns = [c.strip("[] \n") for c in row.split(" ") if not c in ("", "\n")]
+        columns = [None if c == '.' else c for c in columns]
         sorted_crates.append(columns)
+
+    n_stacks = int(data[split_index - 1].strip()[-1])
+
+    stacks = [[] for _ in range(n_stacks)]
+
+    for i in range(len(sorted_crates) - 1, -1, -1):
+        for j, stack in enumerate(stacks):
+            if crate := sorted_crates[i][j]:
+                stack.append(crate)
 
     sorted_instructions = []
 
-    for i in [i.strip() for i in data[split_index + 1 :]]:
+    for instruction in [i.strip() for i in data[split_index + 1:]]:
+        instruction = instruction.replace("move ", "").replace("from ", "").replace("to ", "").strip().split()
+        instruction = [int(i) for i in instruction]
+
         sorted_instructions.append(
-            tuple(int(x.strip()) for x in re.split(r"move|from|to", i) if x)
+            tuple([instruction[0], instruction[1] - 1, instruction[2] - 1])
         )
 
-    return sorted_crates, sorted_instructions
+    return stacks, sorted_instructions
 
 
-def solve_part_1(data: tuple[list]):
+def solve_part_1(data: tuple[list, list]):
+    stacks, instructions = data
+    # print("Start")
+    # for stack in stacks:
+    #     print(stack)
 
-    crates, instructions = data
+    for i, (n_crates, from_i, to_i) in enumerate(instructions, 1):
 
-    n_crates, from_i, to_i = instructions[0]
+        for _ in range(n_crates):
+            if stacks[from_i]:
+                crate = stacks[from_i].pop()
+                stacks[to_i].append(crate)
 
-    for i in range(n_crates):
-        print(crates[i][from_i - 1])
+        # print()
+        # print(f"Step {i}: Move {n_crates} from {from_i+1} to {to_i+1}")
+        # for j, stack in enumerate(stacks, 1):
+        #     print(f"{j} {stack}")
+
+    return ''.join([stack[-1] if stack else ' ' for stack in stacks])
 
 
 def solve_part_2(data):
-    pass
+    stacks, instructions = data
+    # print("Start")
+    # for stack in stacks:
+    #     print(stack)
+
+    for i, (n_crates, from_i, to_i) in enumerate(instructions, 1):
+        buffer = []
+
+        for _ in range(n_crates):
+            if stacks[from_i]:
+                buffer.append(stacks[from_i].pop())
+
+        while buffer:
+            stacks[to_i].append(buffer.pop())
+
+        # print()
+        # print(f"Step {i}: Move {n_crates} from {from_i+1} to {to_i+1}")
+        # for j, stack in enumerate(stacks, 1):
+        #     print(f"{j} {stack}")
+
+    return ''.join([stack[-1] if stack else '' for stack in stacks])
 
 
 if __name__ == "__main__":
--- 2022/05/test_solution.py
+++ 2022/05/test_solution.py
@@ -5,11 +5,11 @@ from solution import load_input, prepare_data, solve_part_1, solve_part_2
 FILE = "sample.txt"
 
 PART_1_SAMPLE_ANSWER = "CMZ"
-PART_2_SAMPLE_ANSWER = None
+PART_2_SAMPLE_ANSWER = "MCD"
 
 
 @pytest.fixture(scope="module")
-def data() -> list:
+def data() -> tuple:
     return prepare_data(load_input(file=FILE))
 
 
@@ -18,6 +18,6 @@ def test_solve_part_1(data, expected=PART_1_SAMPLE_ANSWER):
     assert solve_part_1(data) == expected
 
 
-@pytest.mark.skip()
+# @pytest.mark.skip
 def test_solve_part_2(data, expected=PART_2_SAMPLE_ANSWER):
     assert solve_part_2(data) == expected
