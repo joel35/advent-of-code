e6a9076fd5fc032741fc6a180cce77ccd754275e
joel35 <64959969+joel35@users.noreply.github.com>
2023-12-02 14:58:30 +0000

    2023-02 done

---
+++ 2023/02/cube_conundrum.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+#  https://adventofcode.com/2023/day/2
+
+
+import re
+
+
+FILE = "input.txt"
+
+
+def main():
+    data = load_input()
+    print(f"PART 1: {solve_part_1(data)}")
+    print(f"PART 2: {solve_part_2(data)}")
+
+
+def load_input(file=FILE) -> list:
+    with open(file) as f:
+        return [line.strip() for line in f]
+
+
+def solve_part_1(data):
+    maxes = {
+        'red': 12,
+        'green': 13,
+        'blue': 14
+    }
+    possible = []
+
+    for line in data:
+        game, colours = prepare_line(line)
+        is_possible = True
+
+        for n, colour in colours:
+            if n > maxes[colour]:
+                is_possible = False
+                break
+        
+        if is_possible:
+            possible.append(game)
+
+    return sum(possible)
+
+def solve_part_2(data):
+    powers = []
+
+    for line in data:
+        maxes = {
+            'red': 0,
+            'green': 0,
+            'blue': 0
+        }
+        _, colours = prepare_line(line)
+
+        for n, colour in colours:
+            if n > maxes[colour]:
+                maxes[colour] = n
+        
+        red, green, blue = maxes.values()
+        powers.append(red * green * blue)
+    return sum(powers)
+
+def prepare_line(line: list[str]) -> tuple[int, list]:
+    cleaned = re.split(f', |; |: ', line)
+    game = int(cleaned[0].split()[-1])
+    pairs = []
+
+    for pair in cleaned[1:]:
+        count, colour = pair.split()
+        pairs.append((int(count), colour))
+
+    return game, pairs
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit(0)
