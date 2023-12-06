6e28cdad2ef788cf618113037d01753886239deb
joel35 <64959969+joel35@users.noreply.github.com>
2023-12-06 00:56:17 +0000

    not working

--- 2023/05/if_you_give_a_seed_a_fertilizer.py
+++ 2023/05/if_you_give_a_seed_a_fertilizer.py
@@ -21,8 +21,33 @@ def load_input(file=FILE) -> list:
 
 def solve_part_1(data):
     seeds = get_seeds(data)
-    print(seeds)
-    map = get_map(data)
+    maps = get_map(data)
+    # print(seeds)
+    # print(maps)
+    destinations = []
+
+    sample_destinations = [82, 43, 86, 35]
+
+    for seed, sample in zip(seeds, sample_destinations):
+        history = [seed]
+        mapped_num = seed
+        for map in maps:
+            for dest, source, num in map:
+                if source <= mapped_num <= source + num:
+                    # print(f'{min_n} <= {mapped_num} <= {max_n}')
+                    # print(f'{dest=} {source=} {num=}')
+                    diff = mapped_num - source
+                    # print(f'{mapped_num}-{min_n}={diff}')
+                    mapped_num = dest + diff
+                    # print(f'{dest}+{diff}={mapped_num}')
+                    # print()
+
+            history.append(mapped_num)
+        print(f'{mapped_num}={sample}: {mapped_num==sample}; {history}')
+        destinations.append(mapped_num)
+        # print(history)
+    # print(destinations)
+    return min(destinations)
 
 
 def get_seeds(data: list) -> list:
@@ -37,46 +62,29 @@ def get_seeds(data: list) -> list:
 def get_map(data: list):
     gap_indices = [i + 1 for i, _ in enumerate(data) if _ == "\n"]
 
+    if (last_index := len(data) + 1) not in gap_indices:
+        gap_indices.append(last_index)
+
     maps = [
         data[gap_indices[i - 1] : gap_indices[i] - 1]
         for i in range(1, len(gap_indices))
     ]
 
-    # print(maps)
+    parsed_maps = [parse_map(map) for map in maps]
 
-    map = maps[0]
-    print(map)
+    return parsed_maps
 
-    key = re.split(r'-to-| ', map[0].strip())[:2]
-    print(key)
 
-    source_list = []
-    dest_list = []
+def parse_map(map: list) -> list:
+    key = re.split(r"-to-| ", map[0].strip())[:2]
+    parsed = []
+    # print(key)
 
     for vals in map[1:]:
-        destination, source, num = [int(v) for v in vals.strip().split()]
+        dest, source, num = [int(v) for v in vals.strip().split()]
+        parsed.append([dest, source, num])
 
-        print(source, destination, num)
-
-        for s in range(source, source+num):
-            source_list.append(s)
-        
-        for n in range(max(source_list)):
-            if n not in source_list:
-                source_list.append(n)
-        
-        for d in range(destination, destination + num):
-            dest_list.append(d)
-        
-        for n in range(max(dest_list)):
-            if n not in dest_list:
-                dest_list.append(n)
-        
-    
-    zipped = {s: d for s, d in zip(source_list, dest_list)}
-    print(zipped)
-
-    
+    return parsed
 
 
 def solve_part_2(data):
